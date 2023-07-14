daff62472aa9a4467719ad2b13dfb14ebaa16be8
Ganesh Nayanajith <32520964+ganeshnayanajith@users.noreply.github.com>
2023-07-14 13:27:03 +0530

    user management swagger docs

--- modules/user/user.route.js
+++ modules/user/user.route.js
@@ -3,6 +3,44 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('./user.controller');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: API endpoints for users management
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Register user
+ *     tags: [Users]
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Registration Successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: number
+ *                 email:
+ *                   type: string
+ *                 password:
+ *                   type: string
+ */
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 
